4bf1e1e8366fbc8ec48c18d89bce594254d4b53f
Burkhard Lück <lueck@hube-lueck.de>
2012-04-20 16:52:25 +0200

    load translation catalog libkdegames to make picmi fully translated

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char *argv[])
 
     KCmdLineArgs::init(argc, argv, &about);
     KApplication app;
+    KGlobal::locale()->insertCatalog( QLatin1String( "libkdegames" ));
 
     MainWindow *w = new MainWindow;
     w->show();

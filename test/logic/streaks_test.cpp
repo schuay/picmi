3baa43137f024a178581c43bcca962b2e808c6eb
Jakob Gruber <jakob.gruber@gmail.com>
2013-07-12 23:02:46 +0200

    Unit test infrastructure with an empty test

---
+++ test/logic/streaks_test.cpp
@@ -0,0 +1,10 @@
+#include "streaks_test.h"
+
+#include <QTest>
+
+QTEST_MAIN(StreaksTest)
+
+void StreaksTest::testSanity()
+{
+    QCOMPARE(1, 1);
+}
---
+++ test/logic/streaks_test.h
@@ -0,0 +1,14 @@
+#ifndef __STREAKS_TEST_H
+#define __STREAKS_TEST_H
+
+#include <QObject>
+
+class StreaksTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testSanity();
+};
+
+#endif /* __STREAKS_TEST_H */
